e4e9aef68ef8fa949d18f66af790e6f24d1c4a7c
Akash Mahajan <akashmahajan025@gmail.com>
2023-11-30 22:08:15 -0500

    Add roulette-wheel selection

--- GeneticAlgorithm.java
+++ GeneticAlgorithm.java
@@ -23,9 +23,11 @@ public class GeneticAlgorithm {
         this.makePopulation();
     }
 
-    public GeneticAlgorithm(int populationSize, double mutationRate) {
+    public GeneticAlgorithm(int populationSize, int genomeLength, double mutationRate) {
         // NOTE: Use this constructor when crossover rate must be turned off
         this.populationSize = populationSize;
+        this.genomeLength = genomeLength;
+        this.crossoverRate = 0.0;
         this.mutationRate = mutationRate;
     }
 
@@ -67,6 +69,30 @@ public class GeneticAlgorithm {
         evaluation[0] = totalFitness / this.populationSize;
         evaluation[1] = bestFitness;
         return evaluation;
+    }
+
+    private int[][] rouletteWheel(int n) {
+        int totalFitness = 0;
+        int[] fitnessValues = new int[this.populationSize];
+        int[][] pairs = new int[n][this.genomeLength];
+
+        for (int i = 0; i < this.populationSize; i++) {
+            fitnessValues[i] = fitness(this.population[i]); 
+            totalFitness += fitnessValues[i];
+        }
+        double threshold = this.random.nextDouble() * totalFitness;
+        int cumFitness = 0;
+        int k = 0;
+
+        for (int i = 0; i < n; i++) {
+            cumFitness += fitnessValues[i];
+            if (cumFitness >= threshold && k < n) {
+                pairs[k] = this.population[i];
+                replacement[k] = i;
+                k += 1;
+            }
+        }
+        return pairs;
     }
 
     private int[][] selectPair() {
@@ -125,11 +151,13 @@ public class GeneticAlgorithm {
         for(int i = 0; i < maxGenerations; i++) {
             double[] evaluation = this.evaluateFitness();
             if (evaluation[1] == targetFitness) {
+                this.printGeneration(i + 1);
                 break;
             }
 
             // Pick a pair of individual
-            int[][] genomePair = this.selectPair();
+            // int[][] genomePair = this.selectPair();
+            int[][] genomePair = this.rouletteWheel(2);
 
             // Produce child population
             int[][] childGenome = this.crossover(genomePair[0], genomePair[1]);
